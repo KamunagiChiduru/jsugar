56162ad5fffa886aaf7919c347ddd8f9c8a6ce3b
KamunagiChiduru <twilight_epitaph@hotmail.co.jp>
2012-12-16 01:28:56 +0900

    javadocコメント整備

--- src/jp/michikusa/chitose/swt/widget/Shadow.java
+++ src/jp/michikusa/chitose/swt/widget/Shadow.java
@@ -2,17 +2,33 @@ package jp.michikusa.chitose.swt.widget;
 
 import org.eclipse.swt.SWT;
 
+/**
+ * widgetにつく影のスタイル。<br>
+ * 
+ * @author kamichidu
+ * @version 0.01
+ * @since 2012/12/15
+ */
 public enum Shadow{
-    NONE(SWT.SHADOW_NONE), 
-    IN(SWT.SHADOW_IN), 
-    OUT(SWT.SHADOW_OUT), 
-    ;
+    /**
+     * 影のないスタイル。
+     */
+    NONE(SWT.SHADOW_NONE),
+    /**
+     * 内側に影のつくスタイル。
+     */
+    IN(SWT.SHADOW_IN),
+    /**
+     * 外側に影のつくスタイル。
+     */
+    OUT(SWT.SHADOW_OUT), ;
     
     int style(){
         return this.style;
     }
     
     private final int style;
+    
     private Shadow(int style){
         this.style= style;
     }
