97d24f48a48f8a244d4e3a62b8ae309ad7c8527a
KamunagiChiduru <twilight_epitaph@hotmail.co.jp>
2012-12-16 04:08:27 +0900

    javadoc整備

--- src/jp/michikusa/chitose/annotation/ExceptionSafety.java
+++ src/jp/michikusa/chitose/annotation/ExceptionSafety.java
@@ -6,21 +6,40 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 例外安全性についてドキュメント化するために使う。<br>
+ * 例外安全性について注釈するために使う注釈型。<br>
  * 
- * @author  kamichidu
+ * @author kamichidu
  * @version 0.01
- * @since   2012/10/27
+ * @since 2012/10/27
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.CONSTRUCTOR, ElementType.METHOD})
 public @interface ExceptionSafety{
-	public Level value();
-	
-	public static enum Level{
-		NO_GUARANTEE,
-		BASIC_GUARANTEE,
-		STRONG_GUARANTEE,
-		NO_THROW_GUARANTEE, ;
-	}
+    public Level value();
+    
+    /**
+     * 例外安全性の程度について記述するための列挙型。<br>
+     * 
+     * @author kamichidu
+     * @version 0.01
+     * @since 2012/12/16
+     */
+    public static enum Level{
+        /**
+         * 保証をしない。<br>
+         */
+        NO_GUARANTEE,
+        /**
+         * 基本保証をする。<br>
+         */
+        BASIC_GUARANTEE,
+        /**
+         * 強い保証をする。<br>
+         */
+        STRONG_GUARANTEE,
+        /**
+         * 投げない保証をする。<br>
+         */
+        NO_THROW_GUARANTEE, ;
+    }
 }
